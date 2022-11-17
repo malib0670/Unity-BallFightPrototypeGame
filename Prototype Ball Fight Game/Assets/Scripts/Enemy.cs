8a857aae4ee7848ad4bfa204395775c73410a9b7
Muhammed Ali BUĞDAY <bugdaymali@gmail.com>
2022-11-17 23:13:18 +0300

    Ball Fight Prototype Game
    Ball Fight prototype game were made

---
+++ Prototype Ball Fight Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float speed = 1.0f;
+    private GameObject player;
+    private Rigidbody enemyRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyStartMethod();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyUpdateMethod();
+    }
+
+    public void enemyStartMethod()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    public void enemyUpdateMethod()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
+
+        if (enemyRb.transform.position.y < -10) 
+        {
+            Destroy(gameObject);
+        }
+    }
+}
